74d907478669347d02ffa99672f76585b4ff858a
polyachkov <d.polyachkov@g.nsu.ru>
2024-10-18 21:17:25 +0700

    create project

---
+++ src/main/java/ru/polyachkov/LightCRM/LightCrmApplication.java
@@ -0,0 +1,13 @@
+package ru.polyachkov.LightCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightCrmApplication.class, args);
+	}
+
+}
